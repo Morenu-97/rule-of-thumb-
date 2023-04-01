162473b35525b50eb9145091b83a89bdb3c5460a
Brayan <brayan1897mendez@gmail.com>
2023-04-01 01:39:51 -0500

    card component

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Card.css";
 
-function Card() {
-  return <h2>hjaskdaskl</h2>;
+function Card(props) {
+  return (
+    <div className="pictures">
+      <img src={props.picture} />
+      <div className="title">
+        <button className="icon-button-down">
+          <img src="assets/img/thumbs-down.svg" alt="thumbs down"></img>
+        </button>
+        <p className="card-title">{props.name}</p>
+      </div>
+      <p className="card-description">{props.description}</p>
+      <p className="category">{props.category}</p>
+      <div>
+        <button className="icon-button-up">
+          <img src="assets/img/thumbs-up.svg" alt="thumbs up"></img>
+        </button>
+        <button className="icon-button-down" aria-label="thumbs down">
+          <img src="assets/img/thumbs-down.svg" alt="thumbs down"></img>
+        </button>
+        <button className="icon-vote">Vote now</button>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
