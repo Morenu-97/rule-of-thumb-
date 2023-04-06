b8d5513d6c1e5679ca10dc2a01d60a6b6ac6f15d
Brayan <brayan1897mendez@gmail.com>
2023-04-06 00:06:40 -0500

    Fix: Card bug

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,31 +1,49 @@
 import "./card.css";
 
-function Card({image, name, description, category}) {
-  const imageUrl = require(`assets/img/${image}`); 
+function Card({ image, name, description, category }) {
+  const imageUrl = require(`assets/img/${image}`);
   const divStyle = {
     backgroundImage: `url(${imageUrl})`,
-    width: '300px',
-    height: '300px',
+    width: "300px",
+    height: "300px",
   };
 
   return (
     <div className="card" style={divStyle}>
-      <div className="title">
-        <button className="icon-button-down">
+      <div className="card-header">
+        <button className="icon card-header-icon">
           <img src="assets/img/thumbs-down.svg" alt="thumbs down"></img>
         </button>
         <p className="card-title">{name}</p>
       </div>
       <p className="card-description">{description}</p>
       <p className="category">{category}</p>
-      <div>
-        <button className="icon-button-up">
+      <div className="card-buttons">
+        <button className="icon button-up">
           <img src="assets/img/thumbs-up.svg" alt="thumbs up"></img>
         </button>
-        <button className="icon-button-down" aria-label="thumbs down">
+        <button className="icon button-down" aria-label="thumbs down">
           <img src="assets/img/thumbs-down.svg" alt="thumbs down"></img>
         </button>
         <button className="icon-vote">Vote now</button>
+      </div>
+      <div className="card-footer">
+        <div className="percentage-approval">
+          <img
+            className="percentage-approval_icon"
+            src="assets/img/thumbs-up.svg"
+            alt="thumbs up"
+          ></img>
+          <span>25.5%</span>
+        </div>
+        <div className="percentage-rejection">
+          <span>74.5%</span>
+          <img
+            className="percentage-rejection_icon"
+            src="assets/img/thumbs-down.svg"
+            alt="thumbs down"
+          ></img>
+        </div>
       </div>
     </div>
   );
